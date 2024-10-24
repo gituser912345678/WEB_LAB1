fb4a777a0e294202f2970ec4076cf1c0aafa2ffe
gituser912345678 <138629929+gituser912345678@users.noreply.github.com>
2024-10-24 13:13:05 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ValidationException {
 
         var fcgiInterface = new FCGIInterface();
 
@@ -38,20 +38,27 @@ public class Main {
                 "reason": "%s"
             }
             """;
+            try {
+                var query = System.getProperties().getProperty("QUERY_STRING");
 
-            var query = System.getProperties().getProperty("QUERY_STRING");
+                var request = readRequestBody(query);
+                validateParams(request);
+                long startTime = System.currentTimeMillis();
 
-            var request = readRequestBody(query);
-            long startTime = System.currentTimeMillis();
+                String result = Script.defenitionHit(Integer.parseInt(request.get("x_field")), Integer.parseInt(request.get("y_field")), Float.parseFloat(request.get("R_field")));
 
-            String result = Script.defenitionHit(Integer.parseInt(request.get("x_field")), Integer.parseInt(request.get("y_field")), Float.parseFloat(request.get("R_field")));
+                long endTime = System.currentTimeMillis();
+                String executionTime = String.valueOf(endTime - startTime);
 
-            long endTime = System.currentTimeMillis();
-            String executionTime = String.valueOf(endTime - startTime);
+                var json = String.format(RESULT_JSON, result, executionTime);
+                var response = String.format(HTTP_RESPONSE, json.getBytes(StandardCharsets.UTF_8).length + 2, json);
+                System.out.println(response);
+            } catch (ValidationException exception) {
+                var json = String.format(ERROR_JSON, exception.getMessage());
+                var response = String.format(HTTP_ERROR, json.getBytes(StandardCharsets.UTF_8).length + 2, json);
+                System.out.println(response);
+            }
 
-            var json = String.format(RESULT_JSON, result, executionTime);
-            var response = String.format(HTTP_RESPONSE, json.getBytes(StandardCharsets.UTF_8).length + 2, json);
-            System.out.println(response);
 
 
 
@@ -67,4 +74,49 @@ public class Main {
         }
         return queryPairs;
     }
+
+    private static void validateParams(Map<String, String> params) throws ValidationException {
+        var x = params.get("x_field");
+        if (x == null || x.isEmpty()) {
+            throw new ValidationException("x is invalid");
+        }
+
+        try {
+            var xx = Integer.parseInt(x);
+            if (xx < -5 || xx > 3) {
+                throw new ValidationException("x has forbidden value");
+            }
+        } catch (NumberFormatException e) {
+            throw new ValidationException("x is not a number");
+        }
+
+        var y = params.get("y_field");
+        if (y == null || y.isEmpty()) {
+            throw new ValidationException("y is invalid");
+        }
+
+        try {
+            var yy = Float.parseFloat(y);
+            if (yy < -3 || yy > 5) {
+                throw new ValidationException("y has forbidden value");
+            }
+        } catch (NumberFormatException e) {
+            throw new ValidationException("y is not a number");
+        }
+
+        var r = params.get("R_field");
+        if (r == null || r.isEmpty()) {
+            throw new ValidationException("r is invalid");
+        }
+
+        try {
+            var rr = Float.parseFloat(r);
+            if (rr < 1 || rr > 3) {
+                throw new ValidationException("r has forbidden value");
+            }
+        } catch (NumberFormatException e) {
+            throw new ValidationException("r is not a number");
+        }
+    }
+
 }
